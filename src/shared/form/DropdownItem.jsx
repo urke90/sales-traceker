c134d5b14a9bca5a8a221a6cc43375010b82bcdf
urke90 <urosbijelic90@gmail.com>
2023-01-13 18:19:46 +0100

    removed dark: className from  dropdownItem

--- src/shared/form/DropdownItem.jsx
+++ src/shared/form/DropdownItem.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const DropdownItem = ({ title }) => {
     return (
-        <li className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">
+        <li className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600">
             {title}
         </li>
     );
